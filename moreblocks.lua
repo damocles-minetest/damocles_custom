c46943fbb445df20ca162ad45a6db44492339f96
Thomas Rudin <thomas@rudin.io>
2019-10-02 11:08:00 +0200

    more slippery

--- moreblocks.lua
+++ moreblocks.lua
@@ -3,6 +3,6 @@
 stairsplus:register_all("moreblocks", "ice", "default:ice", {
 	description = "Ice",
 	tiles = {"default_ice.png"},
-	groups = {cracky=3,slippery=3},
+	groups = {cracky=3,slippery=5},
 	sounds = default.node_sound_glass_defaults()
 })
