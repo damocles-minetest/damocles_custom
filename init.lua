3cc00aeb85641245395c477ce3249a7edd49d931
Thomas Rudin <thomas@rudin.io>
2019-10-02 10:46:32 +0200

    ice slope

--- init.lua
+++ init.lua
@@ -2,3 +2,4 @@
 local MP = minetest.get_modpath("damocles_custom")
 
 dofile(MP.."/disable_mobs_spawning.lua")
+dofile(MP.."/moreblocks.lua")
---
+++ moreblocks.lua
@@ -0,0 +1,8 @@
+
+
+stairsplus:register_all("moreblocks", "ice", "default:ice", {
+	description = "Ice",
+	tiles = {"default_ice.png"},
+	groups = {cracky=3,slippery=3},
+	sounds = default.node_sound_glass_defaults()
+})
