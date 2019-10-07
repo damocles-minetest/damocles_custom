7616e9f3136fceb72081be89b7172c22e522179a
BuckarooBanzay <BuckarooBanzay@github.com>
2019-10-07 11:15:24 +0200

    brown mushroom = higher jump

--- food_effects.lua
+++ food_effects.lua
@@ -34,6 +34,6 @@ end
 
 
 register_food_effect("farming:coffee_cup_hot", player_monoids.speed, 2, 5)
-register_food_effect("farming:jaffa_cake", player_monoids.jump, 2, 5)
+register_food_effect("flowers:mushroom_brown", player_monoids.jump, 2, 5)
 register_food_effect("farming:turkish_delight", player_monoids.visual_size, {x=0.3, y=0.3, z=0.3}, 5)
 
