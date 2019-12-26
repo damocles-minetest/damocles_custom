4fb5c72ca034aeee4ef588d19c0faf903afa3aa6
Thomas Rudin <thomas@rudin.io>
2019-12-26 19:16:17 +0100

    add "damocles_center" priv

--- privs.lua
+++ privs.lua
@@ -3,3 +3,6 @@ minetest.register_privilege("damocles_builder", {
   description = "can build games on the server",
 })
 
+minetest.register_privilege("damocles_center", {
+  description = "can build on the center platform",
+})
